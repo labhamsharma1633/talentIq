dadae2e3d888d54d521fc27c82b6e7acc198a8ab
Labham Sharma <git config --global user.email>
2026-03-17 20:26:58 +0530

    fix: update backend URL and debug env issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import SessionPage from "./pages/SessionPage";
 function App() {
   const { isSignedIn, isLoaded } = useUser();
 
+  console.log("BASE URL:", import.meta.env.VITE_BACKEND_URL);
+
   // this will get rid of the flickering effect
   if (!isLoaded) return null;
 
