82112bef6677e68da07b5b195b094b63523908b7
Labham Sharma <git config --global user.email>
2026-03-17 15:25:38 +0530

    fix import case issue

--- frontend/src/hooks/useSessions.js
+++ frontend/src/hooks/useSessions.js
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { sessionApi } from "../api/sessions";
+import { sessionApi } from "../api/Sessions.js";
 
 export const useCreateSession = () => {
   const result = useMutation({
